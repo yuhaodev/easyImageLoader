c3ad0db073091029441cba1120e10c3ef17ba26e
haoyu <haoyuint@163.com>
2017-07-20 14:06:17 +0800

    update

--- imageloader/src/main/java/com/example/imageloader/ImageLoader.java
+++ imageloader/src/main/java/com/example/imageloader/ImageLoader.java
@@ -24,7 +24,7 @@ import java.util.concurrent.Executors;
 
 public class ImageLoader {
     //图片缓存
-    ImageCache mImageCache = new ImageCache();
+    ImageCache mImageCache = new MemoryCache();
     //    LruCache<String,Bitmap> mImageCache;
     //SD卡缓存
     DiskCache mDiskCache = new DiskCache();
@@ -39,6 +39,40 @@ public class ImageLoader {
 //    public ImageLoader() {
 //        initImageCache();
 //    }
+    public void setImageCache(ImageCache cache){
+        mImageCache = cache;
+    }
+
+    public  void displayImage(String imageUrl,ImageView imageView){
+        Bitmap bitmap = mImageCache.get(imageUrl);
+        if(null!=bitmap){
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        //图片没缓存，提交到线程池下载图片
+        submitLoadRequest(imageUrl,imageView);
+
+
+    }
+
+    private void submitLoadRequest(final String ImageUrl,final ImageView imageView){
+
+        imageView.setTag(ImageUrl);
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadImage(ImageUrl);
+                if (null == bitmap) {
+                    return;
+                }
+                if(imageView.getTag().equals(ImageUrl)){
+                    imageView.setImageBitmap(bitmap);
+                }
+                mImageCache.put(ImageUrl,bitmap);
+            }
+        });
+
+    }
 
 //    private void initImageCache(){
 //            //计算可使用的最大内存
@@ -52,55 +86,55 @@ public class ImageLoader {
 //            }
 //        };
 //     }
-    public void setUseDiskCache(boolean useDiskCache){
-        isUseDiskCache = useDiskCache;
-    }
-    public void setmDoubleCache(boolean useDoubleCache){
-        isUseDoubleCache = useDoubleCache;
-    }
+//    public void setUseDiskCache(boolean useDiskCache){
+//        isUseDiskCache = useDiskCache;
+//    }
+//    public void setmDoubleCache(boolean useDoubleCache){
+//        isUseDoubleCache = useDoubleCache;
+//    }
 
-     public void displayImage(final String url, final ImageView imageView){
-         //判断使用哪种缓存方式
-//         Bitmap bitmap = isUseDiskCache ? mDiskCache.get(url) : mImageCache.get(url);
-         Bitmap bitmap = null;
-         if(isUseDoubleCache){
-             bitmap = mDoubleCache.get(url);
-
-         }else if(isUseDiskCache){
-             bitmap = mDiskCache.get(url);
-         }else {
-             bitmap = mImageCache.get(url);
-         }
-
-
-//         Bitmap bitmap=mImageCache.get(url);
-         if(null!=bitmap){
-             imageView.setImageBitmap(bitmap);
-             return;
-         }
-         imageView.setTag(url);
-         Log.e("url", " 图片已经添加tag               " );
-         mExecutorService.submit(new Runnable() {
-             @Override
-             public void run() {
-                 Log.e("url", "null==bitmap;" );
-                 Bitmap bitmap = downloadImage(url);
-                 if(null==bitmap){
-                     Log.e("url", "null==bitmap;" );
-                     return;
-                 }else{
-                     Log.e("url", "null!=bitmap;" );
-                 }
-                 if(imageView.getTag().equals(url)){
-                     imageView.setImageBitmap(bitmap);
-                     Log.e("url", " 图片已经加载上去                coon.disconnect();" );
-                     mImageCache.put(url, bitmap);
-                 }else{
-                     Log.e("url", "!!!!        imageView.getTag().equals(url);" );
-                 }
-             }
-         });
-     }
+//     public void displayImage(final String url, final ImageView imageView){
+//         //判断使用哪种缓存方式
+////         Bitmap bitmap = isUseDiskCache ? mDiskCache.get(url) : mImageCache.get(url);
+//         Bitmap bitmap = null;
+//         if(isUseDoubleCache){
+//             bitmap = mDoubleCache.get(url);
+//
+//         }else if(isUseDiskCache){
+//             bitmap = mDiskCache.get(url);
+//         }else {
+//             bitmap = mImageCache.get(url);
+//         }
+//
+//
+////         Bitmap bitmap=mImageCache.get(url);
+//         if(null!=bitmap){
+//             imageView.setImageBitmap(bitmap);
+//             return;
+//         }
+//         imageView.setTag(url);
+//         Log.e("url", " 图片已经添加tag               " );
+//         mExecutorService.submit(new Runnable() {
+//             @Override
+//             public void run() {
+//                 Log.e("url", "null==bitmap;" );
+//                 Bitmap bitmap = downloadImage(url);
+//                 if(null==bitmap){
+//                     Log.e("url", "null==bitmap;" );
+//                     return;
+//                 }else{
+//                     Log.e("url", "null!=bitmap;" );
+//                 }
+//                 if(imageView.getTag().equals(url)){
+//                     imageView.setImageBitmap(bitmap);
+//                     Log.e("url", " 图片已经加载上去                coon.disconnect();" );
+//                     mImageCache.put(url, bitmap);
+//                 }else{
+//                     Log.e("url", "!!!!        imageView.getTag().equals(url);" );
+//                 }
+//             }
+//         });
+//     }
      public Bitmap downloadImage(String imageUrl){
          Bitmap bitmap=null;
          try {
