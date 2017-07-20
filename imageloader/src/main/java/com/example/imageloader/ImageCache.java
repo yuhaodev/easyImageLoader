ece5170b3458869b12e08db5d855d9871c73f3d9
haoyu <haoyuint@163.com>
2017-07-20 14:05:57 +0800

    update

--- imageloader/src/main/java/com/example/imageloader/ImageCache.java
+++ imageloader/src/main/java/com/example/imageloader/ImageCache.java
@@ -12,32 +12,34 @@ import android.util.LruCache;
  * @Version: v1.0
  */
 
-public class ImageCache {
-    //图片LRU缓存
-    LruCache<String,Bitmap> mImageCache;
-
-    public ImageCache() {
-        initImageCache();
-    }
-
-    private void initImageCache(){
-            //计算可使用的最大内存
-        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        //取四分之一的可用内存为缓存
-        int cacheSize=maxMemory/4;
-        mImageCache=new LruCache<String,Bitmap>(cacheSize){
-            @Override
-            protected int sizeOf(String key, Bitmap bitmap) {
-                return bitmap.getRowBytes()*bitmap.getHeight()/1024;
-            }
-        };
-     }
-     public void put(String url,Bitmap bitmap){
-         mImageCache.put(url, bitmap);
-
-     }
-     public Bitmap get(String url){
-         return mImageCache.get(url);
-     }
+public interface ImageCache {
+//    //图片LRU缓存
+//    LruCache<String,Bitmap> mImageCache;
+//
+//    public ImageCache() {
+//        initImageCache();
+//    }
+//
+//    private void initImageCache(){
+//            //计算可使用的最大内存
+//        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+//        //取四分之一的可用内存为缓存
+//        int cacheSize=maxMemory/4;
+//        mImageCache=new LruCache<String,Bitmap>(cacheSize){
+//            @Override
+//            protected int sizeOf(String key, Bitmap bitmap) {
+//                return bitmap.getRowBytes()*bitmap.getHeight()/1024;
+//            }
+//        };
+//     }
+//     public void put(String url,Bitmap bitmap){
+//         mImageCache.put(url, bitmap);
+//
+//     }
+//     public Bitmap get(String url){
+//         return mImageCache.get(url);
+//     }
+    public Bitmap get(String url);
+    public void put(String url,Bitmap bmp);
 
 }
